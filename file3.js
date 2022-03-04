1275d0961245c5bd75403027d59f30d9d7fcd41c
Carlos Marin <53407930+Cdev02@users.noreply.github.com>
2022-03-03 19:37:36 -0500

    Delete file3.js

--- file3.js
+++
@@ -1,7 +0,0 @@
-console.log('This is a new feature added for the feat2 branch');
-setTimeout(() => {
-  console.log('This message will appear each 5 seconds');
-}, 5000);
-const testFunction = () => {
-  console.log('tHIS IS AN ARROW FUNCTION');
-};
