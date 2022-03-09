f83ae2a25bc9487a00d4590d88ad11bf37bd576c
YoimarMoreno <yoimar.moreno@outlook.es>
2022-03-08 19:23:55 -0500

    Patch method added v2

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const elementsRouter = require(`${__dirname}/routes/elementsRouter`);
 app.use(express.json());
-app.use('/api/v1/elements', elementsRouter);
+app.use('/api/v1/elementos', elementsRouter);
 module.exports = app;
