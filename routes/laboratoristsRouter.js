fa3d63003c410a88fa8f9d8c58a4b0bc50b7fbe0
Yoimar <Yoimar.moreno@outlook.es>
2022-04-21 10:08:49 -0500

    Cambios realizados para que los campos coincidan con los de la BD

--- routes/laboratoristsRouter.js
+++ routes/laboratoristsRouter.js
@@ -7,12 +7,12 @@ const db = require(`${__dirname}/../db/dbConnection`);
 //Crear un laboratorista
 router.post("/", async (req, res) => {
   try {
-    const { cedula, nombre, apellido } = req.body;
+    const { usuario, contraseña, nombre, apellido } = req.body;
     const newElement = await db.query(
       `INSERT INTO laboratorista
-         (cedula, nombre, apellido) 
-         VALUES ($1, $2, $3)`,
-      [cedula, nombre, apellido]
+         (usuario, contraseña, nombre, apellido) 
+         VALUES ($1, $2, $3, $4)`,
+      [usuario, contraseña, nombre, apellido]
     );
     res.json({ status: "Success", newElement });
   } catch (error) {
@@ -33,10 +33,10 @@ router.get("/", async (req, res) => {
 //Consultar un laboratorista en particular
 router.get("/:id", async (req, res) => {
   try {
-    const { cedula } = req.params;
+    const { usuario } = req.params;
     const element = await db.query(
-      "SELECT * FROM laboratorista WHERE cedula	= $1",
-      [cedula]
+      "SELECT * FROM laboratorista WHERE usuario	= $1",
+      [usuario]
     );
     res.json({ status: "success", data: element.rows[0] });
   } catch (error) {
@@ -47,10 +47,10 @@ router.get("/:id", async (req, res) => {
 //Actualizar la informaión de un laboratorista
 router.patch("/:id", async (req, res) => {
   try {
-    const { nombre, apellido } = req.body;
+    const { contraseña, nombre, apellido } = req.body;
     const newElement = await db.query(
-      `UPDATE laboratorista nombre = $1, apellido = $2 WHERE cedula = $3`,
-      [nombre, apellido]
+      `UPDATE laboratorista contraseña = $1, nombre = $2, apellido = $3 WHERE usuario = $4`,
+      [contraseña, nombre, apellido]
     );
     res.json({ status: "Laboratorista actualizado", data: newElement });
   } catch (error) {
@@ -61,13 +61,12 @@ router.patch("/:id", async (req, res) => {
 //Eliminar un laboratorista
 router.delete("/:id", async (req, res) => {
   try {
-    const { cedula } = req.params;
-    await db.query("DELETE FROM laboratorista WHERE cedula = $1", [cedula]);
+    const { usuario } = req.params;
+    await db.query("DELETE FROM laboratorista WHERE usuario = $1", [usuario]);
     res.json({ status: "Laboratorista eliminado" });
   } catch (error) {
     console.error(error.message);
   }
 });
-
 
 module.exports = router;
