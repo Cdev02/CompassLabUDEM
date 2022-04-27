efe5d9d2de0aac2f72848c1dfc34d1bff9fbd6d2
Carlos Marin <carlosdaniel.marin202@gmail.com>
2022-04-26 20:28:08 -0500

    Se agregan dos parametros a las urls de cada ruta y se refactorizan algunas partes del codigo

--- routes/LabCargoRouter.js
+++ routes/LabCargoRouter.js
@@ -1,11 +1,11 @@
-const Router = require("express-promise-router");
+const Router = require('express-promise-router');
 const router = new Router();
 const db = require(`${__dirname}/../db/dbConnection`);
 
 // Aquí van los routes para la tabla laboratoristas_a_cargo
 
 //Crear un registro
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const { id_laboratorio, usuario, descripcion } = req.body;
     const newElement = await db.query(
@@ -14,69 +14,63 @@ router.post("/", async (req, res) => {
            VALUES ($1, $2, $3)`,
       [id_laboratorio, usuario, descripcion]
     );
-    res.json({ status: "Success", newElement });
+    res.json({ status: 'Success', newElement });
   } catch (error) {
     console.error(error.message);
   }
 });
 
-//Consultar todos los laboratoristas a cargo (Es necesario?)
-router.get("/", async (req, res) => {
+//Consultar todos los laboratoristas a cargo
+router.get('/', async (req, res) => {
   try {
-    const allElements = await db.query("SELECT * FROM laboratorista_a_cargo");
-    res.json({ status: "success", data: allElements.rows });
+    const allElements = await db.query('SELECT * FROM laboratorista_a_cargo');
+    res.json({ status: 'success', data: allElements.rows });
   } catch (error) {
     console.error(error.message);
   }
 });
 
 //Consultar un laboratorista a cargo en particular
-router.get("/:id", async (req, res) => {
+router.get('/:id/:idLab', async (req, res) => {
   try {
-    const { id_laboratorio, usuario } = req.params;
+    const { id, idLab } = req.params;
     const element = await db.query(
-      "SELECT * FROM laboratorista_a_cargo WHERE id_laboratorio = $1 AND usuario = $2",
-      [id_laboratorio, usuario]
+      'SELECT * FROM laboratorista_a_cargo WHERE id_laboratorio = $1 AND usuario = $2',
+      [idLab, id]
     );
-    res.json({ status: "success", data: element.rows[0] });
+    res.json({ status: 'success', data: element.rows[0] });
   } catch (error) {
     console.error(error.message);
   }
 });
 
-//Actualizar la informaión de un laboratorista a cargo
-router.patch("/:id", async (req, res) => {
-    try {
-      const {
-        id_laboratorio,
-        usuario,
-        descripcion,
-      } = req.body;
-      const newElement = await db.query(
-        `UPDATE laboratorista_a_cargo id_laboratorio = $1, usuario = $2, descripcion = $3 WHERE id_laboratorio = $4 AND usuario = $5`,
-        [
-            id_laboratorio,
-            usuario,
-            descripcion,
-        ]
-      );
-      res.json({ status: "Laboratorista a cargo actualizado", data: newElement });
-    } catch (error) {
-      console.error(error.message);
-    }
-  });
+//Actualizar la información de un laboratorista a cargo
+router.patch('/:id/:idLab', async (req, res) => {
+  try {
+    const { id, idLab } = req.params;
+    const { desc, newId, newLabId } = req.body;
+    const newElement = await db.query(
+      `UPDATE laboratorista_a_cargo id_laboratorio = $1, usuario = $2, descripcion = $3 WHERE id_laboratorio = $4 AND usuario = $5`,
+      [newId, newLabId, desc, idLab, id]
+    );
+    res.json({ status: 'Laboratorista a cargo actualizado', data: newElement });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 //Eliminar un registro
-router.delete("/:id", async (req, res) => {
-    try {
-      const {id_laboratorio, usuario, } = req.params;
-      await db.query("DELETE FROM laboratorista_a_cargo WHERE WHERE id_laboratorio = $1 AND usuario = $2", [codigo_ficha, codigo_elemento]);
-      res.json({ status: "Laboratorista a cargo eliminado" });
-    } catch (error) {
-      console.error(error.message);
-    }
-  });
+router.delete('/:id/:idLab', async (req, res) => {
+  try {
+    const { id, idLab } = req.params;
+    await db.query(
+      'DELETE FROM laboratorista_a_cargo WHERE WHERE id_laboratorio = $1 AND usuario = $2',
+      [idLab, id]
+    );
+    res.json({ status: 'Laboratorista a cargo eliminado' });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 module.exports = router;
-
-
