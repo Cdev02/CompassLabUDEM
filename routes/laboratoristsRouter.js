2d287f5b7fbfb34b4ba7a833082412751779523e
Carlos Marin <carlosdaniel.marin202@gmail.com>
2022-04-26 20:32:39 -0500

    se reescribio los parametros de las rutas de id a user y se corrigieron algunas partes del codigo

--- routes/laboratoristsRouter.js
+++ routes/laboratoristsRouter.js
@@ -1,69 +1,66 @@
-const Router = require("express-promise-router");
+const Router = require('express-promise-router');
 const router = new Router();
 const db = require(`${__dirname}/../db/dbConnection`);
 
 // Aquí van los routes para la tabla laboratoristas
 
 //Crear un laboratorista
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const { usuario, contraseña, nombre, apellido } = req.body;
+    const { usuario, contrasena, nombre, apellido } = req.body;
     const newElement = await db.query(
       `INSERT INTO laboratorista
-         (usuario, contraseña, nombre, apellido) 
+         (usuario, contrasena, nombre, apellido) 
          VALUES ($1, $2, $3, $4)`,
-      [usuario, contraseña, nombre, apellido]
+      [usuario, contrasena, nombre, apellido]
     );
-    res.json({ status: "Success", newElement });
+    res.json({ status: 'Success', newElement });
   } catch (error) {
     console.error(error.message);
   }
 });
 
 //Consultar todos los laboratoristas
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const allElements = await db.query("SELECT * FROM laboratorista");
-    res.json({ status: "success", data: allElements.rows });
+    const allElements = await db.query('SELECT * FROM laboratorista');
+    res.json({ status: 'success', data: allElements.rows });
   } catch (error) {
     console.error(error.message);
   }
 });
 
 //Consultar un laboratorista en particular
-router.get("/:id", async (req, res) => {
+router.get('/:user', async (req, res) => {
   try {
-    const { usuario } = req.params;
-    const element = await db.query(
-      "SELECT * FROM laboratorista WHERE usuario	= $1",
-      [usuario]
-    );
-    res.json({ status: "success", data: element.rows[0] });
+    const element = await db.query('SELECT * FROM laboratorista WHERE usuario	= $1', [
+      req.params.user,
+    ]);
+    res.json({ status: 'success', data: element.rows[0] });
   } catch (error) {
     console.error(error.message);
   }
 });
 
-//Actualizar la informaión de un laboratorista
-router.patch("/:id", async (req, res) => {
+//Actualizar la información de un laboratorista
+router.patch('/:user', async (req, res) => {
   try {
-    const { contraseña, nombre, apellido } = req.body;
+    const { contrasena, nombre, apellido } = req.body;
     const newElement = await db.query(
-      `UPDATE laboratorista contraseña = $1, nombre = $2, apellido = $3 WHERE usuario = $4`,
-      [contraseña, nombre, apellido]
+      `UPDATE laboratorista contrasena = $1, nombre = $2, apellido = $3 WHERE usuario = $4`,
+      [contrasena, nombre, apellido, req.params.user]
     );
-    res.json({ status: "Laboratorista actualizado", data: newElement });
+    res.json({ status: 'Laboratorista actualizado', data: newElement });
   } catch (error) {
     console.error(error.message);
   }
 });
 
 //Eliminar un laboratorista
-router.delete("/:id", async (req, res) => {
+router.delete('/:user', async (req, res) => {
   try {
-    const { usuario } = req.params;
-    await db.query("DELETE FROM laboratorista WHERE usuario = $1", [usuario]);
-    res.json({ status: "Laboratorista eliminado" });
+    await db.query('DELETE FROM laboratorista WHERE usuario = $1', [req.params.user]);
+    res.json({ status: 'Laboratorista eliminado' });
   } catch (error) {
     console.error(error.message);
   }
